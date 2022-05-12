108b4099dfd6410bae21f2c74c8cafc4d184d4c7
Asad Mahmood <asadmhmd1234@gmail.com>
2022-05-12 14:11:41 +0530

    Add and remove the placeholder content for the body of the project

---
+++ script.js
@@ -0,0 +1,25 @@
+const movieForm = document.getElementById("movie-form");
+const inputEl = document.getElementById("movie");
+const emptyEl = document.getElementById("empty");
+const noResultEl = document.getElementById("no-result");
+
+movieForm.addEventListener("submit", getMoviesBySearch);
+
+async function getMoviesBySearch(event) {
+  event.preventDefault();
+  const movieName = inputEl.value;
+  try {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=866de549&s=${movieName}`
+    );
+    const data = await response.json();
+    console.log(data);
+    let i = data.Search.length >= 5 ? 5 : data.Search.length;
+    console.log(i);
+    emptyEl.classList.add("hidden");
+  } catch (error) {
+    console.log("Oops, something went terribly wrong!!!");
+    emptyEl.classList.add("hidden");
+    noResultEl.classList.remove("hidden");
+  }
+}
